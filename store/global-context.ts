1c30ca213608f65442e2bc43e8d88d0b1a832820
ibrahim <i.aliyu019@gmail.com>
2025-01-08 20:58:21 +0100

    feat: enhance sign-in flow and update dashboard with user data

--- store/global-context.ts
+++ store/global-context.ts
@@ -5,13 +5,19 @@ import { useEffect, useState } from "react";
 
 function useGlobalStore() {
   const [loading, setLoading] = useState(true);
+  //Used to store student data before they finish registration on the app
   const [studentData, setStudentData] = useState<StudentData>();
+  //Used to store user Data after successful login
+
+  const [userData, setUserData] = useState<StudentData>();
 
   useEffect(() => {}, []);
 
   return {
     studentData,
     setStudentData,
+    userData,
+    setUserData,
   };
 }
 
