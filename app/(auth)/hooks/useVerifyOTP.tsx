01d18355c520179ae1999da6e49b503e85222e23
jamal <135423558+jamal-codez@users.noreply.github.com>
2025-08-30 22:22:09 +0100

    fix: packages

--- app/(auth)/hooks/useVerifyOTP.tsx
+++ app/(auth)/hooks/useVerifyOTP.tsx
@@ -1,12 +1,12 @@
 import ReactDOMServer from "react-dom/server";
 import { useMutation } from "@tanstack/react-query";
-import { Resend } from "resend";
-import PlaidVerifyIdentityEmail from "@/emails/plaid-verify-identity";
+// import { Resend } from "resend";
+// import PlaidVerifyIdentityEmail from "@/emails/plaid-verify-identity";
 import { requestOTP, verifyOTP } from "../services";
 import { RequestOTPPayload, VerifyOTP } from "../types";
 import { router } from "expo-router";
 
-const resend = new Resend("re_CWgXYEW4_AeMvnU5vhCDXeF5L7AUaW2FQ");
+// const resend = new Resend("re_CWgXYEW4_AeMvnU5vhCDXeF5L7AUaW2FQ");
 
 export function useVerifyOTP() {
   const mutation = useMutation({
